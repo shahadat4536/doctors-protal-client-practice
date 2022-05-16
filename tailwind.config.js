54239944a3bb05908c9e619959d0efa5c2246805
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-16 09:29:22 +0600

    71-2

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        lite: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
