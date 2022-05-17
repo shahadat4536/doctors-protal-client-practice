e5626572b1cd1e9a1d27026784a59b2836069031
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-17 11:03:56 +0600

    73-3

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
 import auth from "../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   if (user) {
     console.log(user);
   }
@@ -12,7 +15,32 @@ const Login = () => {
       <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="text-center text-2xl">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: "error message",
+                  },
+                })}
+              />
+              <label class="label">
+                <span class="label-text-alt">Alt label</span>
+              </label>
+            </div>
 
+            <input />
+            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+            <input type="number" {...register("age", { min: 18, max: 99 })} />
+            <input type="submit" />
+          </form>
           <div class="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
