4eb648fbea529ac30ecf7a5bc42018a63b2cbb80
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-20 08:52:35 +0600

    77-4

--- src/Pages/DashBoard/Payment.js
+++ src/Pages/DashBoard/Payment.js
@@ -2,7 +2,13 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../Shared/Loading";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe(
+  "pk_test_51L1LUkCdaLiCtPNZgNYqZ9z0GEVKqa9SHtw8G3xMMBtJAPrlR9jJQpUh3HMQKSGZjWAuK35HlMzN76QbcrnBuF5w00qUa6eAMj"
+);
 const Payment = () => {
   const { id } = useParams();
   const url = `http://localhost:5000/booking/${id}`;
@@ -37,7 +43,11 @@ const Payment = () => {
         </div>
       </div>
       <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-        <div class="card-body"></div>
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
       </div>
     </div>
   );
